e5d8199ec6fc18c8e579300e68e3b481a30740f0
Kevin Yang <yangjiun0314@gmail.com>
2025-04-10 23:37:42 +0900

    🔁 5강 반복문 실습 코드 업로드

---
+++ 05_loops/gugudan_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            printf("%d x %d = %d\t", i, j, i * j);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
