af384174de10801d0174c00af6c77c1c5af4789a
Kevin Yang <yangjiun0314@gmail.com>
2025-04-10 23:35:46 +0900

    🔁 5강 반복문 실습 코드 업로드

---
+++ 05_loops/infinity_loop_2.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    for(int i = 0; i <= 100; i--) {
+        printf("Hello World!\n");
+    }
+    system("pasue");
+    return 0;
+}
---
+++ 05_loops/while_loop.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    char a;
+    scanf("%d %c", &n, &a);
+    while (n--) {
+        printf("%c ", a); 
+    } 
+    return 0;
+}
